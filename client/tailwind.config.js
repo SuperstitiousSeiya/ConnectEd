1b7dc4bdca1c0de86be1994b55c7417c9bae64f7
SuperstitiousSeiya <reyver.abellanosa@student.passerellesnumeriques.org>
2023-12-15 02:03:16 +0800

    Added dashboard

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#00359F",
+        semiBg: "#E4EFF9",
+      }
+    },
+  },
+  plugins: [],
+}
